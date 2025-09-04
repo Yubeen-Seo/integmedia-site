49779447e5366281b678d7ffa11f22867157ecba
Yubeen Seo <ybseo@integmedia.kr>
2025-09-04 21:51:17 +0900

    fix(router): wrap App with BrowserRouter to avoid null basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 // C:\Users\ahxh1\OneDrive\Desktop\integmedia-site\src\main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'   // ⬅ 추가
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">       {/* ⬅ Router로 감싸기 */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
